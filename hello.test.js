d0cfe5d043ff569bcecbccfe679622ec28303bbf
Guido Delgado <guido.delgado@gmail.com>
2022-11-21 21:13:03 -0600

    Se actualiza la prueba de unidad

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,8 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from Spain");
+    expect(hello.hello()).toEqual(
+      "Hola Mundo desde Costa Rica, el miercoles 23 de Noviembre jugamos contra Espanna en el Mundial de Qatar 2022"
+    );
   });
 });
